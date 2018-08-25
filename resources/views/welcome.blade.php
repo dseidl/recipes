03bc1cf89af97f57571ccb9b3a458f82e370aed0
David Seidl <david.seidl@towa.at>
2018-08-25 18:07:17 +0200

    feat: scaffolding auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Recipes
                 </div>
 
                 <div class="links">
